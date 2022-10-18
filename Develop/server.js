bc8e557416652b2f517887b4183c80a5d6b6d2fb
V Mendez <victormendez990@gmail.com>
2022-10-17 23:28:04 -0600

    Add to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,20 @@ const express = require('express');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Get Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+// Listener and notifier of functioning port
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
