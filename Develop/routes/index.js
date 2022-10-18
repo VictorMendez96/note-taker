df54395f93384a0a5e714534ace466c674edee54
V Mendez <victormendez990@gmail.com>
2022-10-18 00:00:54 -0600

    Add to server.js and created routes folder

---
+++ Develop/routes/index.js
@@ -0,0 +1,8 @@
+// import required modules
+const db = require('../db/db.json');
+const express = require('express');
+
+// Initiate express server
+const app = express()
+
+module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,27 @@
 //Import required modules
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js')
 
+// Create PORT either from .env or port 3001
 const PORT = process.env.PORT || 3001;
 
 // initiate express server
 const app = express();
 
 // Use express middleware for parsing JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
-// Get Route for homepage
+app.use(express.static('public'));
+
+// Get Route for index.html page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-// GET Route for notes page
+// GET Route for notes.html page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
